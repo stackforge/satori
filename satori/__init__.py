7ac1d418e14583d7f10be480791c9ef349be36bc
Ziad Sawalha <gerrit@highbridgellc.com>
2014-03-07 11:03:23 -0600

    Update tests and testing requirements
    - pep257 with our patch (beyond >=0.3.1)
    - flake8_docstring with our patch (>=0.2.0)
    - don't need Babel or WebOb yet; remove
    - sort alphabetically where order is not important
    - indicate python 3/3.3 compatibility; classifiers
    - document running coverage report
    - add coverage and virtual env paths to .gitignore
    - fix coverage filtering to not include tests
    - tweak pylintrc
    
    Change-Id: I0e840a424bebd35d349a21ab769f47da7b34a02d

--- satori/__init__.py
+++ satori/__init__.py
@@ -9,11 +9,10 @@
 #   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #   License for the specific language governing permissions and limitations
 #   under the License.
-#
 
 """Satori main module."""
 
-__all__ = ['__version__']
+__all__ = ('__version__')
 
 import pbr.version
 
