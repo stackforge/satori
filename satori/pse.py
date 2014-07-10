4ee574543e2bafe2c9a7da78b009dfd229652777
Samuel Stavinoha <samuel.stavinoha@rackspace.com>
2014-07-10 12:21:37 -0500

    Extends bp/windows-support submission 98383
    Move support for PoSH-Ohai into its own provider module.
    Raise UnsupportedPlatform exceptions in ohai_solo.py
    when the client is non-linux, and raise the same
    exception in posh_ohai.py when the client is non-windows.
    
    Define a unicode function via utils.py which is python2
    and python3 compatible. Replace the unicode function
    previously defined in ohai_solo.py with this.
    
    Many style fixes, and trimming some code.
    Fixes test_dns.py from change in python-whois.
    Updates pep8 requirement from jcrocholl/pep8 Issue#240:
        Fix was merged into pep8 1.5.7
    Updates hacking requirement to fix VersionConflict
    
    Change-Id: I0b33d632d157d5219bfee25b669408885f03b873
    Implements: blueprint windows-support

--- satori/pse.py
+++ satori/pse.py
@@ -10,10 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""Windows remote client module implemented using psexec.py.
-
-bla
-"""
+"""Windows remote client module implemented using contrib/psexec.py."""
 
 import eventlet
 eventlet.monkey_patch()
@@ -95,7 +92,7 @@ class PSE(object):  # pylint: disable=R0902
         self.ssh_tunnel = None
         self._substituted_command = None
 
-        #creating temp file to talk to _process with
+        # creating temp file to talk to _process with
         self._file_write = tempfile.NamedTemporaryFile()
         self._file_read = open(self._file_write.name, 'r')
 
@@ -155,7 +152,7 @@ class PSE(object):  # pylint: disable=R0902
         This will tunnel a local ephemeral port to the host's port.
         This will preserve the original host and port
         """
-        self.ssh_tunnel = tunnel.connect(self.host, self.port, self.gateway)
+        self.ssh_tunnel = tunnel.Tunnel(self.host, self.port, self.gateway)
         self._orig_host = self.host
         self._orig_port = self.port
         self.host, self.port = self.ssh_tunnel.address
