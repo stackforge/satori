1c852eab64f8ec9ee6fb64ccd60f8ca39003d242
Samuel Stavinoha <samuel.stavinoha@rackspace.com>
2014-07-23 15:04:43 +0000

    Adds Windows Support to satori discovery
    This change adds the capability to satori to do data plane
    discovery on Windows devices. For that, a class has been
    added that mirrors the functionality of satori/ssh.py and
    utilizes a "3rd party" script (satori/contrib/psexec.py)
    which is called via subprocess.Popen(). Further to that, an
    SSH tunneling class has been put in place that uses paramiko
    to establish a tunnel (similar to running ssh -L from a shell).
    requirements.txt has been extended to include impacket which
    satori/contrib/psexec.py imports.
    
    Move support for PoSH-Ohai into its own provider module.
    Raise UnsupportedPlatform exceptions in ohai_solo.py
    when the client is non-linux, and raise the same
    exception in posh_ohai.py when the client is non-windows.
    
    Define a unicode function via utils.py which is python2
    and python3 compatible. Replace the unicode function
    previously defined in ohai_solo.py with this.
    
    Co-Authored-By: Nico Engelen <engelen.nico@googlemail.com>
    Co-Authored-By: Samuel Stavinoha <samuel.stavinoha@rackspace.com>
    Change-Id: I7a94eea9446bc7f57843407fb98880222f7af6af
    Implements: blueprint windows-support

--- satori/tests/test_sysinfo_ohai_solo.py
+++ satori/tests/test_sysinfo_ohai_solo.py
@@ -48,119 +48,124 @@ class TestOhaiSolo(utils.TestCase):
 
 class TestOhaiInstall(utils.TestCase):
 
+    def setUp(self):
+        super(TestOhaiInstall, self).setUp()
+        self.mock_remotesshclient = mock.MagicMock()
+        self.mock_remotesshclient.is_windows.return_value = False
+
     def test_install_remote_fedora(self):
-        mock_ssh = mock.MagicMock()
         response = {'exit_code': 0, 'foo': 'bar'}
-        mock_ssh.execute.return_value = response
-        result = ohai_solo.install_remote(mock_ssh)
+        self.mock_remotesshclient.execute.return_value = response
+        result = ohai_solo.install_remote(self.mock_remotesshclient)
         self.assertEqual(result, response)
-        self.assertEqual(mock_ssh.execute.call_count, 3)
-        mock_ssh.execute.assert_has_calls([
+        self.assertEqual(self.mock_remotesshclient.execute.call_count, 3)
+        self.mock_remotesshclient.execute.assert_has_calls([
             mock.call('sudo wget -N http://ohai.rax.io/install.sh', wd='/tmp'),
             mock.call('sudo bash install.sh', wd='/tmp', with_exit_code=True),
             mock.call('sudo rm install.sh', wd='/tmp')])
 
-    def test_install_remote_failed(self):
-        mock_ssh = mock.MagicMock()
+    def test_install_linux_remote_failed(self):
         response = {'exit_code': 1, 'stdout': "", "stderr": "FAIL"}
-        mock_ssh.execute.return_value = response
+        self.mock_remotesshclient.execute.return_value = response
         self.assertRaises(errors.SystemInfoCommandInstallFailed,
-                          ohai_solo.install_remote, mock_ssh)
+                          ohai_solo.install_remote, self.mock_remotesshclient)
 
 
 class TestOhaiRemove(utils.TestCase):
 
+    def setUp(self):
+        super(TestOhaiRemove, self).setUp()
+        self.mock_remotesshclient = mock.MagicMock()
+        self.mock_remotesshclient.is_windows.return_value = False
+
     def test_remove_remote_fedora(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.is_debian.return_value = False
-        mock_ssh.is_fedora.return_value = True
+        self.mock_remotesshclient.is_debian.return_value = False
+        self.mock_remotesshclient.is_fedora.return_value = True
         response = {'exit_code': 0, 'foo': 'bar'}
-        mock_ssh.execute.return_value = response
-        result = ohai_solo.remove_remote(mock_ssh)
+        self.mock_remotesshclient.execute.return_value = response
+        result = ohai_solo.remove_remote(self.mock_remotesshclient)
         self.assertEqual(result, response)
-        mock_ssh.execute.assert_called_once_with(
+        self.mock_remotesshclient.execute.assert_called_once_with(
             'sudo yum -y erase ohai-solo', wd='/tmp')
 
     def test_remove_remote_debian(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.is_debian.return_value = True
-        mock_ssh.is_fedora.return_value = False
+        self.mock_remotesshclient.is_debian.return_value = True
+        self.mock_remotesshclient.is_fedora.return_value = False
         response = {'exit_code': 0, 'foo': 'bar'}
-        mock_ssh.execute.return_value = response
-        result = ohai_solo.remove_remote(mock_ssh)
+        self.mock_remotesshclient.execute.return_value = response
+        result = ohai_solo.remove_remote(self.mock_remotesshclient)
         self.assertEqual(result, response)
-        mock_ssh.execute.assert_called_once_with(
+        self.mock_remotesshclient.execute.assert_called_once_with(
             'sudo dpkg --purge ohai-solo', wd='/tmp')
 
     def test_remove_remote_unsupported(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.is_debian.return_value = False
-        mock_ssh.is_fedora.return_value = False
+        self.mock_remotesshclient.is_debian.return_value = False
+        self.mock_remotesshclient.is_fedora.return_value = False
         self.assertRaises(errors.UnsupportedPlatform,
-                          ohai_solo.remove_remote, mock_ssh)
+                          ohai_solo.remove_remote, self.mock_remotesshclient)
 
 
 class TestSystemInfo(utils.TestCase):
 
+    def setUp(self):
+        super(TestSystemInfo, self).setUp()
+        self.mock_remotesshclient = mock.MagicMock()
+        self.mock_remotesshclient.is_windows.return_value = False
+
     def test_system_info(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.execute.return_value = {
+        self.mock_remotesshclient.execute.return_value = {
             'exit_code': 0,
             'stdout': "{}",
             'stderr': ""
         }
-        ohai_solo.system_info(mock_ssh)
-        mock_ssh.execute.assert_called_with("sudo -i ohai-solo")
+        ohai_solo.system_info(self.mock_remotesshclient)
+        self.mock_remotesshclient.execute.assert_called_with(
+            "sudo -i ohai-solo")
 
     def test_system_info_with_motd(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.execute.return_value = {
+        self.mock_remotesshclient.execute.return_value = {
             'exit_code': 0,
             'stdout': "Hello world\n {}",
             'stderr': ""
         }
-        ohai_solo.system_info(mock_ssh)
-        mock_ssh.execute.assert_called_with("sudo -i ohai-solo")
+        ohai_solo.system_info(self.mock_remotesshclient)
+        self.mock_remotesshclient.execute.assert_called_with("sudo -i ohai-solo")
 
     def test_system_info_bad_json(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.execute.return_value = {
+        self.mock_remotesshclient.execute.return_value = {
             'exit_code': 0,
             'stdout': "{Not JSON!}",
             'stderr': ""
         }
         self.assertRaises(errors.SystemInfoNotJson, ohai_solo.system_info,
-                          mock_ssh)
+                          self.mock_remotesshclient)
 
     def test_system_info_missing_json(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.execute.return_value = {
+        self.mock_remotesshclient.execute.return_value = {
             'exit_code': 0,
             'stdout': "No JSON!",
             'stderr': ""
         }
         self.assertRaises(errors.SystemInfoMissingJson, ohai_solo.system_info,
-                          mock_ssh)
+                          self.mock_remotesshclient)
 
     def test_system_info_command_not_found(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.execute.return_value = {
+        self.mock_remotesshclient.execute.return_value = {
             'exit_code': 1,
             'stdout': "",
             'stderr': "ohai-solo command not found"
         }
         self.assertRaises(errors.SystemInfoCommandMissing,
-                          ohai_solo.system_info, mock_ssh)
+                          ohai_solo.system_info, self.mock_remotesshclient)
 
     def test_system_info_could_not_find(self):
-        mock_ssh = mock.MagicMock()
-        mock_ssh.execute.return_value = {
+        self.mock_remotesshclient.execute.return_value = {
             'exit_code': 1,
             'stdout': "",
             'stderr': "Could not find ohai-solo."
         }
         self.assertRaises(errors.SystemInfoCommandMissing,
-                          ohai_solo.system_info, mock_ssh)
+                          ohai_solo.system_info, self.mock_remotesshclient)
 
 
 if __name__ == "__main__":
